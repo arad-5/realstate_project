89c9a7417740be7ea560f1f742319d29a53ec783
Arad5 <arad5@outlook.com>
2022-05-10 22:15:34 -0700

    landing page banners & hit propeties in landing made responsive

--- components/Property.jsx
+++ components/Property.jsx
@@ -1,20 +1,81 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import {User} from "@nextui-org/react"
+import Link from 'next/link';
+import Image from 'next/image';
+import { User } from '@nextui-org/react';
 
+import { FaBed, FaBath } from 'react-icons/fa';
+import { MdCalendarViewMonth, MdVerified } from 'react-icons/md';
+import { IoLocationSharp } from 'react-icons/io5';
 
-const Property = ({ property: { coverPhoto, price, rentrequency, rooms, title, baths, area, agency, isVerified, externalID } }) => {
-    coverPhoto && console.log(coverPhoto.url)
-    // https://bayut-production.s3.eu-central-1.amazonaws.com/image/183960260/ab96a6f51e074a408a6020a84d614e62
+const Property = ({
+    property: {
+        coverPhoto,
+        price,
+        rooms,
+        title,
+        baths,
+        area,
+        agency,
+        isVerified,
+        externalID,
+        location,
+    },
+}) => {
     return (
         <Link href={`/property/${externalID}`} passHref>
-            <div className=' overflow-hidden rounded-xl cursor-pointer h-96 border-b-4 border-blue-600'>
-                <div className='h-60 relative mb-3'>{coverPhoto && <Image src={coverPhoto.url} layout='fill' alt={title} className='object-cover block rounded-b-md'/>}</div>
-                <h2 className='pl-2'>{title.length > 30 ? <>{title.slice(0, 30)} <span className='text-slate-400'>...</span></> : title}</h2>
-               <Image src={agency?.logo?.url} width="200" height="50" alt='' className='object-contain object-left rounded-md'/>
+            <div className='cursor-pointer rounded-xl border p-3 sm:mt-20 sm:flex-col sm:rounded-2xl sm:shadow-lg bg-white z-10'>
+                <div className='flex justify-between sm:block'>
+                    <div className='relative mb-3 h-28 w-1/3 sm:-mt-20 sm:h-48 sm:w-full'>
+                        {coverPhoto && (
+                            <Image
+                                src={coverPhoto.url}
+                                layout='fill'
+                                alt={title}
+                                className='block rounded-md object-cover sm:rounded-2xl'
+                            />
+                        )}
+                    </div>
+                    <div className='relative my-1 ml-4 w-2/3 overflow-auto sm:ml-0 sm:h-24 sm:w-full'>
+                        <h2>{title.slice(0, 50)}</h2>
+                        <div className='mt-2 flex items-start'>
+                            <IoLocationSharp className='mt-1 text-xl' />
+                            <span>{`${location[2].name}`}</span>
+                        </div>
+                    </div>
+                </div>
+                <div className='flex justify-between'>
+                    <div className='space-x-2 space-y-1'>
+                        <span className='inline-flex items-center rounded-full border px-3 py-1'>
+                            <MdCalendarViewMonth className='mr-2' />
+                            {Math.round(area)} m<sup>2</sup>
+                        </span>
+                        <span className='inline-flex items-center rounded-full border px-3 py-1'>
+                            <FaBed className='mr-2' />
+                            {rooms}
+                        </span>
+                        <span className='inline-flex items-center rounded-full border px-3 py-1'>
+                            <FaBath className='mr-2' />
+                            {baths}
+                        </span>
+                    </div>
+                </div>
+                <div className='text-md mt-2 flex items-center border-t pt-2'>
+                    <div className='rounded-full border px-2 py-1'>
+                        {price} AED
+                    </div>
+                    <div className='ml-auto flex items-center rounded-full border px-2 py-1'>
+                        <Image
+                            src={agency?.logo?.url}
+                            width='30'
+                            height='30'
+                            alt=''
+                            className='block rounded-md object-contain object-left sm:rounded-none sm:rounded-b-md'
+                        />
+                        {isVerified && <MdVerified className='ml-2 text-2xl' />}
+                    </div>
+                </div>
             </div>
         </Link>
-    )
-}
+    );
+};
 
-export default Property
+export default Property;
--- layout/index.js
+++ layout/index.js
@@ -1,13 +1,13 @@
-import Footer from './Footer'
-import Navbar from './Navbar'
+import Footer from './Footer';
+import Navbar from './Navbar';
 const index = ({ children }) => {
     return (
-        <main className='dark'>
-            {/* <Navbar /> */}
-            <div className='min-h-screen dark:bg-[#0a0a0a] dark:text-slate-50 px-14'>{children}</div>
-            {/* <Footer /> */}
-        </main>
-    )
-}
+        <>
+            <Navbar />
+            <main className='container mx-auto px-2 sm:px-14'>{children}</main>
+            <Footer />
+        </>
+    );
+};
 
-export default index
+export default index;
