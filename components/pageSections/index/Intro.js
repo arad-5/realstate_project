dc0eba56964a323c7879f95edc1812830a1f5f90
Arad5 <arad5@outlook.com>
2022-07-21 06:52:51 -0700

    avoid user from zooming

--- components/pageSections/index/Intro.js
+++ components/pageSections/index/Intro.js
@@ -8,7 +8,7 @@ const Intro = () => {
     return (
         <section>
             <div className='relative left-1/2  h-screen w-screen -translate-x-1/2'>
-                <div className='relative z-50 flex h-full w-1/2 flex-col items-center justify-center rounded-tr-[15vw] bg-white after:absolute after:-right-[15vw] after:bottom-0 after:-z-10 after:h-[15vw]  after:w-[15vw] after:rounded-bl-full after:shadow-[-100px_100px_0_100px_white] dark:bg-neutral-900 after:dark:shadow-[-100px_100px_0_100px_#171717]'>
+                <div className='relative z-50 flex h-1/2 sm:h-full sm:w-1/2 flex-col items-center justify-center rounded-tr-[15vw] bg-white after:absolute after:-right-[15vw] after:bottom-0 after:-z-10 after:h-[15vw] after:w-[15vw] after:rounded-bl-full after:shadow-[-100px_100px_0_100px_white] dark:bg-neutral-900 after:sm:block after:hidden after:dark:shadow-[-100px_100px_0_100px_#171717]'>
                     <div>
                         <div className='mb-4'>
                             <h1 className='mb-5 rounded-xl text-[5rem] font-semibold leading-[4rem]'>
@@ -31,7 +31,7 @@ const Intro = () => {
                         </div>
                     </div>
                 </div>
-                <div className='absolute right-0 top-0 -z-10 h-full md:w-4/6'>
+                <div className='absolute right-0 top-0 -z-10 h-full w-full md:w-4/6'>
                     <Image src={'/images/sale-banner.jpg'} layout='fill' className='object-cover' alt='properties for sale' priority />
                 </div>
                 {/* <div className='absolute top-0 left-0 w-full bg-gradient-to-t from-white via-transparent to-transparent transition-colors dark:from-[#151515]'></div> */}
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
-import '../styles/globals.css';
+import '../styles/globals.css'
 
-import { NextUIProvider } from '@nextui-org/react';
-import Layout from '../layout';
-import { ThemeProvider } from 'next-themes';
-import FiltersContext from '../context/search/FiltersProvider';
-import SelectedFiltersSlugProvider from '../context/search/SelectedFiltersSlugProvider';
+import { NextUIProvider } from '@nextui-org/react'
+import Layout from '../layout'
+import { ThemeProvider } from 'next-themes'
+import FiltersContext from '../context/search/FiltersProvider'
+import SelectedFiltersSlugProvider from '../context/search/SelectedFiltersSlugProvider'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-    
     return (
         // 2. Use at the root of your app
         <ThemeProvider attribute='class'>
@@ -15,13 +15,16 @@ function MyApp({ Component, pageProps }) {
                 <FiltersContext>
                     <SelectedFiltersSlugProvider>
                         <Layout>
+                            <Head>
+                                <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' />
+                            </Head>
                             <Component {...pageProps} />
                         </Layout>
                     </SelectedFiltersSlugProvider>
                 </FiltersContext>
             </NextUIProvider>
         </ThemeProvider>
-    );
+    )
 }
 
-export default MyApp;
+export default MyApp
