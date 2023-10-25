5156a16c4c47331c8169bb3df4c1d63e3a297a53
arad <arad5@outlook.com>
2023-10-26 03:16:05 +0330

    coverimage blur data

--- components/property/PropertyCard.jsx
+++ components/property/PropertyCard.jsx
@@ -1,18 +1,32 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import { FaBed, FaBath } from 'react-icons/fa'
-import { MdCalendarViewMonth, MdVerified } from 'react-icons/md'
-import { IoLocationSharp } from 'react-icons/io5'
-import { useEffect } from 'react'
-import Badge from '../Badge'
-import placeholdeImage from '../../public/images/rent-banner.jpg'
+import Link from 'next/link';
+import Image from 'next/image';
+import { FaBed, FaBath } from 'react-icons/fa';
+import { MdCalendarViewMonth, MdVerified } from 'react-icons/md';
+import { IoLocationSharp } from 'react-icons/io5';
+import { useEffect } from 'react';
+import Badge from '../Badge';
+import placeholdeImage from '../../public/images/rent-banner.jpg';
 // components
-import PropertyAgencyLogoBadge from '../agency/PropertyAgencyLogoBadge'
+import PropertyAgencyLogoBadge from '../agency/PropertyAgencyLogoBadge';
 
-const PropertyCard = ({ property: { coverPhoto, price, rooms, title, baths, area, agency, isVerified, externalID, location, purpose } }) => {
+const PropertyCard = ({
+    property: {
+        coverPhoto,
+        price,
+        rooms,
+        title,
+        baths,
+        area,
+        agency,
+        isVerified,
+        externalID,
+        location,
+        purpose,
+    },
+}) => {
     return (
         <Link href={`/property?id=${externalID}`} passHref>
-            <div className='property-card relative z-10 cursor-pointer border-b p-3 py-6 dark:border-neutral-700 last:border-b-0 dark:bg-[#202020] sm:mt-20 sm:rounded-2xl sm:border-0 sm:shadow-lg sm:hover:-translate-y-3 sm:hover:shadow-xl'>
+            <div className='property-card relative z-10 cursor-pointer border-b p-3 py-6 last:border-b-0 dark:border-neutral-700 dark:bg-[#202020] sm:mt-20 sm:rounded-2xl sm:border-0 sm:shadow-lg sm:hover:-translate-y-3 sm:hover:shadow-xl'>
                 <div className='flex justify-between sm:block'>
                     <div className='property-card-image relative mb-3 h-28 w-1/2 rounded-2xl bg-neutral-200 transition dark:bg-neutral-800 sm:-mt-20 sm:h-48 sm:w-full'>
                         {coverPhoto && (
@@ -22,6 +36,7 @@ const PropertyCard = ({ property: { coverPhoto, price, rooms, title, baths, area
                                 className='h-full w-full rounded-xl object-cover sm:rounded-2xl'
                                 layout='fill'
                                 quality={1}
+                                placeholder='blur'
                             />
                         )}
                     </div>
@@ -34,7 +49,7 @@ const PropertyCard = ({ property: { coverPhoto, price, rooms, title, baths, area
                         <IoLocationSharp className='h-5 w-5 flex-shrink-0 text-xl text-blue-600' />
                         {location
                             .slice(1)
-                            .map(location => location.name)
+                            .map((location) => location.name)
                             .join(', ')}
                     </div>
                     <div className='my-2 space-x-2 duration-100'>
@@ -52,24 +67,34 @@ const PropertyCard = ({ property: { coverPhoto, price, rooms, title, baths, area
                         </Badge>
                     </div>
                 </div>
-                <div className='text-md flex items-center justify-between overflow-hidden sm:border-t pt-2 dark:border-[#404040]'>
+                <div className='text-md flex items-center justify-between overflow-hidden pt-2 dark:border-[#404040] sm:border-t'>
                     <div className='flex items-center'>
                         <Badge
                             className={`property-badge property-purpose-badge left-6 top-8 border-0 font-hanson transition duration-700 sm:absolute sm:-mt-20 ${
-                                purpose === 'for-sale' ? 'bg-red-600' : 'bg-yellow-500'
+                                purpose === 'for-sale'
+                                    ? 'bg-red-600'
+                                    : 'bg-yellow-500'
                             } `}
                         >
-                            <span className='tracking-wider text-white'>{purpose === 'for-sale' ? 'SALE' : 'RENT'}</span>
+                            <span className='tracking-wider text-white'>
+                                {purpose === 'for-sale' ? 'SALE' : 'RENT'}
+                            </span>
                         </Badge>
                         <div className='ml-2 font-hanson text-neutral-600 dark:text-neutral-200'>
-                            {price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')} AED
+                            {price
+                                .toString()
+                                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}{' '}
+                            AED
                         </div>
                     </div>
-                    <PropertyAgencyLogoBadge agency={agency} isVerified={isVerified} />
+                    <PropertyAgencyLogoBadge
+                        agency={agency}
+                        isVerified={isVerified}
+                    />
                 </div>
             </div>
         </Link>
-    )
-}
+    );
+};
 
-export default PropertyCard
+export default PropertyCard;
