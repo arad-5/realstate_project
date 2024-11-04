0463bc55030887cf3eda560ea88f5f0a808b7692
arad-5 <arad5@outlook.com>
2024-11-04 07:09:25 -0800

    asdf

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['bayut-production.s3.eu-central-1.amazonaws.com'],
-    unoptimized: true,
-  }
+    reactStrictMode: true,
+    images: {
+        domains: ['bayut-production.s3.eu-central-1.amazonaws.com'],
+        unoptimized: false,
+    },
 }
 
 module.exports = nextConfig
