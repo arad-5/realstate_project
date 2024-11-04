5f09ba128a8d70538c9144a0e2bf7207aa0341b4
arad-5 <arad5@outlook.com>
2024-11-04 07:20:29 -0800

    footer text

--- layout/Footer.jsx
+++ layout/Footer.jsx
@@ -4,14 +4,10 @@ import { FiChevronRight } from 'react-icons/fi'
 
 const Footer = () => {
     return (
-        <footer className="container mx-auto flex items-center justify-center space-x-2 py-4 font-hanson sm:px-10">
+        <footer className="container mx-auto flex items-center justify-center space-x-2 py-4 sm:px-10">
             <div>
-                <div className="text-md flex cursor-pointer items-center space-x-2 rounded-full p-4 transition-colors hover:bg-neutral-50 dark:hover:bg-neutral-800">
-                    <div className="leading-4">
-                        written with ❤️
-                        <br /> by Arad Taghikhani
-                    </div>
-                    <FiChevronRight className="text-2xl text-blue-500" />
+                <div className="text-md flex cursor-pointer items-center space-x-2 rounded-full p-4 transition-colors hover:bg-neutral-50 dark:text-neutral-200 dark:hover:bg-neutral-800">
+                    <div className="leading-4">written by Arad Taghikhani</div>
                 </div>
             </div>
         </footer>
